5369cfd003be5a1896e5cea42214069ec6ddaf95
atkachenko <atkachenko@mercury.io>
2022-11-15 21:02:54 -0500

    initial commit

---
+++ src/main/kotlin/com/tinkooladik/Application.kt
@@ -0,0 +1,14 @@
+package com.tinkooladik
+
+import com.tinkooladik.plugins.configureRouting
+import com.tinkooladik.plugins.configureSerialization
+import io.ktor.server.application.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
